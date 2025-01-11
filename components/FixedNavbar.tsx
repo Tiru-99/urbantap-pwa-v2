63bd1bc2e25cf8f298cff6ee44d91afe60003765
Tiru-99 <aayushtirmanwar1234@gmail.com>
2025-01-11 22:58:18 +0530

    fixed the alignment issue

--- components/FixedNavbar.tsx
+++ components/FixedNavbar.tsx
@@ -22,6 +22,7 @@ export default function FixedNavbar() {
                 src="/assets/urbantap.png" 
                 alt="App logo" 
                 className="w-20 h-20 object-contain px-1"
+                
                 onError={(e) => {
                   e.currentTarget.src = 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>'
                 }}
@@ -48,7 +49,7 @@ export default function FixedNavbar() {
             >
               <X className="h-5 w-5" />
               <span className="sr-only">Close banner</span>
-            </Button>
+            </Button> 
           </div>
         </div>
       </div>
